f285b03ff6213fa5d2cd8f1e2a5c3d47e8a28238
phonikg <jgerryts@gmail.com>
2015-07-27 19:53:46 -0400

    Update geth-installer.sh

--- geth-installer.sh
+++ geth-installer.sh
@@ -9,26 +9,6 @@ sudo apt-get -y upgrade
 echo -e "\e[34mInstalling Dependencies...\e[0m"
 sudo apt-get -y install openssh-server nano screen dphys-swapfile build-essential libgmp3-dev golang git
 
-#Install GO 1.4.2
-echo -e "\e[34mInstalling GO 1.4.2 from source...\e[0m"
-
-#Set environment variables:
-
-cd ~
-export PATH=$HOME/golang/bin:$PATH
-export GOPATH=$HOME/go
-mkdir -p $GOPATH
-
-#Install golang from source:
-
-cd ~
-git clone https://go.googlesource.com/go golang
-cd golang
-git checkout go1.4.2
-cd src
-sudo ./make.bash
-export PATH=$HOME/golang/bin:$PATH
-
 #Install go-ethereum OR `geth`
 echo -e "\e[34mInstalling go-ethereum...\e[0m"
 
