be5e6830a7051a687c1c8ae620a125f8594bc19b
phonikg <jgerryts@gmail.com>
2015-07-28 13:12:56 -0400

    Update geth-installer.sh

--- geth-installer.sh
+++ geth-installer.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-#First we need to perform some housekeeping & install dependencies
-echo -e "\e[32mFirst we need to perform some housekeeping & install dependencies\e[0m"
+#Upgrade Debian to "stretch"
+echo -e "\e[32mFirst we need to edit our sources.list to set stretch/testing as our source so we can install latest versions of dependencies\e[0m"
+sudo sed -i /deb/s/wheezy/stretch/g /etc/apt/sources.list
 echo -e "\e[34mPerforming Housekeeping - Update...\e[0m"
 sudo apt-get -y update
 echo -e "\e[34mPerforming Housekeeping - Upgrade...\e[0m"
 sudo apt-get -y upgrade
+echo -e "\e[34mPerforming Debian Upgrade from `Jessie` to `Stretch`...\e[0m"
+sudo apt-get -y dist-upgrade 
+
+#Install dependencies
 echo -e "\e[34mInstalling Dependencies...\e[0m"
-sudo apt-get -y install openssh-server nano screen dphys-swapfile build-essential libgmp3-dev golang git
+sudo apt-get -y install dphys-swapfile build-essential libgmp3-dev golang git 
 
 #Install go-ethereum OR `geth`
 echo -e "\e[34mInstalling go-ethereum...\e[0m"
-
 cd ~
 git clone https://github.com/ethereum/go-ethereum
 cd go-ethereum
